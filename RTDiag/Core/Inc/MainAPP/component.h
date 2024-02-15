9de2177f15fdd46bd2bce353d639df52828dbddd
teyebhamdi <hamditeyeb1999@gmail.com>
2024-02-15 23:52:35 +0100

    add file .gitignore and file component.h

--- RTDiag/Core/Inc/MainAPP/component.h
+++ RTDiag/Core/Inc/MainAPP/component.h
@@ -1,13 +1,18 @@
-/*
- * component.h
- *
- *  Created on: 31 janv. 2024
- *      Author: Hamdi
- */
 
 #ifndef INC_MAINAPP_COMPONENT_H_
 #define INC_MAINAPP_COMPONENT_H_
-
+/*
+ * Supported device types for this project.
+ */
+typedef enum {
+	lm35,
+	Moteur,
+	Ultrason,
+	arduino,
+	tcrt ,
+	qtr_8rc,
+	dht11,
+}Devicetype;
 
 
 #endif /* INC_MAINAPP_COMPONENT_H_ */
