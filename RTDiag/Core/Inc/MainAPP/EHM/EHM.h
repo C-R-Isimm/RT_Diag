7b61e53b6b99ffc644a90e06f381cb5f2be574f2
Omar <154700885+OmarBannour@users.noreply.github.com>
2024-02-18 14:35:31 +0100

    prototype function for Error handle manager

--- RTDiag/Core/Inc/MainAPP/EHM/EHM.h
+++ RTDiag/Core/Inc/MainAPP/EHM/EHM.h
@@ -2,12 +2,40 @@
  * EHM.h
  *
  *  Created on: Jan 31, 2024
- *      Author: Hamdi
+ *      Author: Omar
  */
 
-#ifndef INC_MAINAPP_EHM_EHM_H_
-#define INC_MAINAPP_EHM_EHM_H_
+#ifndef EHM_H_
+#define EHM_H_
 
+#include "MainAPP/Common/component.h"
 
+typedef enum
+{                       // Enumeration for different types of errors
+    NO_ERROR,           // No error
+    CONNECTION_ERROR,   // Connection error
+    POWER_SUPPLY_ERROR, // Power supply error
+    OVERHEATING_ERROR,  // Overheating error
+    MECHANICAL_FAILURE  // Mechanical failure
+} ErrorType;
+
+/**
+ * @brief   Function to handle sensors
+ *
+ * @param SensorValues    Pointer to the array of sensor values
+ * @param Sensor          Type of device (sensor) being handled
+ * @return ErrorType      ErrorType indicating the status of sensor handling
+ */
+ErrorType EHMHandleSensors(uint8_t *SensorValues, Devicetype Sensor);
+
+/**
+ * @brief  Function to handle motors
+ *
+ * @param MotorValues     Pointer to the array of motor values
+ * @return ErrorType      ErrorType indicating the status of motor handling
+ */
+ErrorType EHMHandleMotor(uint8_t *MotorValues);
 
 #endif /* INC_MAINAPP_EHM_EHM_H_ */
+
+
--- RTDiag/Core/Src/MainAPP/EHM/EHM.c
+++ RTDiag/Core/Src/MainAPP/EHM/EHM.c
@@ -2,7 +2,35 @@
  * EHM.c
  *
  *  Created on: Jan 31, 2024
- *      Author: Hamdi
+ *      Author: omar
  */
+
+#include <stdint.h>
+#include "MainAPP/EHM/EHM.h"
+
+/**
+ *
+ *
+ * @brief   Implementation of HandleSensors function goes here
+ *          You should add the logic to handle sensor values based on the device type
+ * @param SensorValues
+ * @param Sensor
+ * @return ErrorType
+ */
+
+ErrorType EHMHandleSensors(uint8_t* SensorValues, Devicetype Sensor) {
+
+ }
+
+/**
+ * @brief   Implementation of EHMMotor function goes here
+	       You should add the logic to handle motor values
+ *
+ * @param MotorValues
+ * @return ErrorType
+ */
+ ErrorType EHMHandleMotor(uint8_t* MotorValues) {
+
+ }
 
 
