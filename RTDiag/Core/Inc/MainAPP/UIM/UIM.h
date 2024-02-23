44e1973d5cc61aea93617481735b06a70e3d75a6
MariemGHOUDI <mariemghoudi3@gmail.com>
2024-02-23 22:57:37 +0100

    Prototype_Function

--- RTDiag/Core/Inc/MainAPP/UIM/UIM.h
+++ RTDiag/Core/Inc/MainAPP/UIM/UIM.h
@@ -8,6 +8,12 @@
 #ifndef INC_MAINAPP_UIM_UIM_H_
 #define INC_MAINAPP_UIM_UIM_H_
 
+#include "stm32f4xx_hal.h"
+#include "stdint.h"
 
+// Function declarations
+void UIM_Init(void);
+void UIM_Transmit(uint8_t *data, uint16_t length);
+void UIM_Receive(uint8_t *buffer, uint16_t length);
 
 #endif /* INC_MAINAPP_UIM_UIM_H_ */
--- RTDiag/Core/Src/MainAPP/UIM/UIM.c
+++ RTDiag/Core/Src/MainAPP/UIM/UIM.c
@@ -1,8 +1,31 @@
-/*
- * UIM.c
- *
- *  Created on: Jan 31, 2024
- *      Author: Hamdi
- */
+#include "MainAPP/UIM/UIM.h"
+#include "stm32f4xx_hal.h"
 
+//UART_HandleTypeDef huart2;
 
+void UIM_Init(void) {
+    // Configuration of UART parameters (baud rate, etc.)
+    /*huart2.Instance = USART2;
+    huart2.Init.BaudRate = 115200;
+    huart2.Init.WordLength = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits = UART_STOPBITS_1;
+    huart2.Init.Parity = UART_PARITY_NONE;
+    huart2.Init.Mode = UART_MODE_TX_RX;
+    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE; //hardware flow control
+    huart2.Init.OverSampling = UART_OVERSAMPLING_16;*/
+
+    // Initialization of UART with configured parameters
+    /*if (HAL_UART_Init(&huart2) != HAL_OK) {
+        Error_Handler();
+    }*/
+}
+
+void UIM_Transmit(uint8_t *data, uint16_t length) {
+    // Transmission of data via UART
+    //HAL_UART_Transmit(&huart2, data, length, HAL_MAX_DELAY);
+}
+
+void UIM_Receive(uint8_t *buffer, uint16_t length) {
+    // Reception of data via UART
+    //HAL_UART_Receive(&huart2, buffer, length, HAL_MAX_DELAY);
+}
