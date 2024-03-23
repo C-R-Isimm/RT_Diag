783abd1b0f2ac1b36a4c2136d98306e1662f922b
BechirGARA <garabechir06@gmail.com>
2024-03-23 17:55:27 +0100

    ADC Configuration

--- RTDiag/Core/Inc/MainAPP/ComM/ComM.h
+++ RTDiag/Core/Inc/MainAPP/ComM/ComM.h
@@ -5,9 +5,14 @@
  *      Author: garab
  */
 
-#ifndef SRC_MAINAPP_COMM_COMM_H_
-#define SRC_MAINAPP_COMM_COMM_H_
+#ifndef SRC_MAINAPP_COMM_H_
+
+#define SRC_MAINAPP_COMM_H_
+
 #include"MainAPP/Common/component.h"
+
+#include <stdint.h>
+
 void ComMReadSensorValue(Devicetype Device);
 
 void ComMConfigDevice(Devicetype Device);
--- RTDiag/Core/Src/MainAPP/ComM/ComM.c
+++ RTDiag/Core/Src/MainAPP/ComM/ComM.c
@@ -5,7 +5,6 @@
  *      Author: Hamdi wel prezzz
  */
 
-
 #include"MainAPP/ComM/ComM.h"
 
 
@@ -15,14 +14,34 @@ static void ComMConfigUART(){
 
 static void ComMConfigADC(){
 
+	ADC_HandleTypeDef hadc1;
+	void SystemClock_Config(void);
+	static void MX_GPIO_Init(void);
+	static void MX_ADC1_Init(void);
+	int main(void)
+	{
+
+	  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	  HAL_Init();
+
+	  /* Configure the system clock */
+	  SystemClock_Config();
+
+	  /* Initialize all configured peripherals */
+	  MX_GPIO_Init();
+	  MX_ADC1_Init();
+
+	}
+
 }
 
 static void ComMConfigGPIO(){
 
 }
 
-static void ReadGPIO(){
+static uint16_t ReadGPIO(){
 
+	return 0;
 }
 
 static void WriteGPIO(){
@@ -32,53 +51,151 @@ static void WriteGPIO(){
 void ComMConfigDevice(Devicetype Device){
 	if (Device == lm35){
 		ComMConfigADC();
-
 	}
+
 	else if (Device == tcrt){
 		ComMConfigGPIO();
+	}
 
-		}
 	else if (Device == Ultrasonic){
 		ComMConfigGPIO();
+	}
 
-		}
 	else if (Device == arduino){
 		ComMConfigUART();
+	}
 
-		}
 	else if (Device == qtr_8rc){
 		ComMConfigGPIO();
+	}
 
-		}
 	else if (Device == dht11){
 		ComMConfigADC();
+		}
+	else { //don't do anything
+		}
+	}
 
-			}
-}
+
 void ComMReadSensorValue(Devicetype Device){
 
 	if (Device == lm35){
 
 		}
-		else if (Device == tcrt){
 
+	else if (Device == tcrt){
+
+		}
+
+	else if (Device == Ultrasonic){
+
+		}
+
+	else if (Device == arduino){
+
+		}
+
+	else if (Device == qtr_8rc){
+
+		}
+
+	else if (Device == dht11){
 
 			}
-		else if (Device == Ultrasonic){
-
-
-			}
-		else if (Device == arduino){
-
-
-			}
-		else if (Device == qtr_8rc){
-
-
-			}
-		else if (Device == dht11){
-
-
-				}
+	//return 0;
 }
 
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_OFF;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+static void MX_ADC1_Init(void)
+{
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+static void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+}
+
+void Error_Handler(void)
+{
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
