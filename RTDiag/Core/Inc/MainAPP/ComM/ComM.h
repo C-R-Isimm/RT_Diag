e31c088cc992d4afef8f2dd59529d3c7ddee18a6
BechirGARA <garabechir06@gmail.com>
2024-03-23 17:55:27 +0100

    there is error in hal_adc.h path

--- RTDiag/Core/Inc/MainAPP/ComM/ComM.h
+++ RTDiag/Core/Inc/MainAPP/ComM/ComM.h
@@ -6,11 +6,17 @@
  */
 
 #ifndef SRC_MAINAPP_COMM_COMM_H_
+
 #define SRC_MAINAPP_COMM_COMM_H_
+
 #include"MainAPP/Common/component.h"
+
+#include <stdint.h>
+
 void ComMReadSensorValue(Devicetype Device);
 
 void ComMConfigDevice(Devicetype Device);
 
+void Error_Handler(void);
 
 #endif /* SRC_MAINAPP_COMM_COMM_H_ */
