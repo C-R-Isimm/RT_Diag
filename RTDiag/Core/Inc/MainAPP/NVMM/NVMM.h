fd70e4b6da389c743dab0c1cf5dbc28b62f564ce
teyebhamdi <hamditeyeb1999@gmail.com>
2024-04-06 23:32:33 +0000

    save ,read and erase function implementation

--- RTDiag/Core/Inc/MainAPP/NVMM/NVMM.h
+++ RTDiag/Core/Inc/MainAPP/NVMM/NVMM.h
@@ -1,13 +1,27 @@
-/*
- * NVMM.h
- *
- *  Created on: Jan 31, 2024
- *      Author: Hamdi
+#ifndef NVMM_H_
+#define NVMM_H_
+#include "stdint.h"
+#include "MainAPP/Common/component.h"
+#include "string.h"
+
+/**
+ * @brief Data structure for storing device information.
+ * 
+ * This structure defines a data stock entry, which includes the status of the device,
+ * the type of component, and a comment field for additional information.
  */
+typedef struct {
+    // status ;
+    Devicetype component;
+    int8_t comment[100];
+} data_stock;
 
-#ifndef INC_MAINAPP_NVMM_NVMM_H_
-#define INC_MAINAPP_NVMM_NVMM_H_
+uint32_t NvSaveData(data_stock *);
+
+data_stock* NvReadData(uint8_t numofdata);
 
 
+uint32_t NvEraseData(void);
 
-#endif /* INC_MAINAPP_NVMM_NVMM_H_ */
+#endif 
+
