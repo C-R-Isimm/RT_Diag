c69e2ce4b92f92ff697c5ff372c123ca93f8f145
Mariem GHOUDI <149330854+MariemGHOUDI@users.noreply.github.com>
2024-04-14 18:37:25 +0100

    Implemented UIM functions for reading from and writing to the serial terminal via USB

--- RTDiag/Core/Inc/MainAPP/UIM/UIM.h
+++ RTDiag/Core/Inc/MainAPP/UIM/UIM.h
@@ -1,13 +1,13 @@
-/*
- * UIM.h
- *
- *  Created on: Jan 31, 2024
- *      Author: Hamdi
- */
-
 #ifndef INC_MAINAPP_UIM_UIM_H_
 #define INC_MAINAPP_UIM_UIM_H_
 
+#include "usb_device.h"
+#include "usbd_cdc_if.h"
+#include "string.h"
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal.h"
 
-
+// Function declaration
+uint8_t UIM_Transmit_FS(uint8_t* Buf, uint16_t Len);
+void UIM_Receive_FS(uint8_t* Buf, uint32_t *Len);
 #endif /* INC_MAINAPP_UIM_UIM_H_ */
--- RTDiag/Core/Src/MainAPP/UIM/UIM.c
+++ RTDiag/Core/Src/MainAPP/UIM/UIM.c
@@ -1,8 +1,26 @@
-/*
- * UIM.c
- *
- *  Created on: Jan 31, 2024
- *      Author: Hamdi
- */
+#include "MainAPP/UIM/UIM.h"
 
+extern USBD_HandleTypeDef hUsbDeviceFS;
+extern uint8_t buffer[64];
 
+uint8_t UIM_Transmit_FS(uint8_t* Buf, uint16_t Len)
+{
+  uint8_t result = USBD_OK;
+  USBD_CDC_HandleTypeDef *hcdc = (USBD_CDC_HandleTypeDef*)hUsbDeviceFS.pClassData;
+  if (hcdc->TxState != 0){
+    return USBD_BUSY;
+  }
+  USBD_CDC_SetTxBuffer(&hUsbDeviceFS, Buf, Len);
+  result = USBD_CDC_TransmitPacket(&hUsbDeviceFS);
+  return result;
+}
+
+void UIM_Receive_FS(uint8_t* Buf, uint32_t *Len)
+{
+	USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
+	 USBD_CDC_ReceivePacket(&hUsbDeviceFS);
+	  uint8_t len = (uint8_t) *Len;
+	  memset (buffer,'\0', 64),
+	  memcpy (buffer, Buf, len);
+	  //return (USBD_OK);
+}
